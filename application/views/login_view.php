70c46efa77770bf525a76cc5e78de955f5c04978
william chenta <william.chenta@gmail.com>
2013-05-25 00:23:21 -0300

    Implementando login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,9 +1,9 @@
 <div class="container">
 
-<form class="form-signin" style="max-width: 300px; padding: 19px 29px 29px; margin: 0 auto 20px; background-color: #FFFFFF; border: 1px solid #E5E5E5;">
+<form id="frmLogin" name="frmLogin" method="post" action="login" class="form-signin" style="max-width: 300px; padding: 19px 29px 29px; margin: 0 auto 20px; background-color: #FFFFFF; border: 1px solid #E5E5E5;">
   <h2 class="form-signin-heading">Acesso ao sistema</h2>
-  <input type="text" placeholder="Login" class="input-block-level">
-  <input type="password" placeholder="Senha" class="input-block-level">
+  <input id="login" name="login" type="text" placeholder="Login" class="input-block-level">
+  <input type="password" id="senha" name="senha" placeholder="Senha" class="input-block-level">
   <button type="submit" class="btn btn-large btn-primary">Entrar</button>
 </form>
 
