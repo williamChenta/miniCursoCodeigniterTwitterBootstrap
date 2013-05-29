e3682de13a474e60b241427628c26bc47e0fa788
william chenta <william.chenta@gmail.com>
2013-05-29 20:27:32 -0300

    adicionando comentários aos fontes

--- application/controllers/clientes.php
+++ application/controllers/clientes.php
@@ -7,13 +7,19 @@ class Clientes extends Main_Controller {
 
   public function index() {
 
+    //carrega a sessão do usuário para verificar se o mesmo tem permissão a esta página. senão, redireciona para login
     $arr_sessao = $this->session->all_userdata();
 
     if ($arr_sessao['grupo'] <> 1) {
       redirect('login');
     }
 
-    //verifica se quer incluir
+    /***
+     * verifica se quer incluir.
+     * pega todos os dados post. 
+     * o primeiro parâmetro indica o nome do campo post. se estiver null retorna todos os dados post
+     * o segundo parâmetro indica que é para sanitizar os dados. XSS filter
+     */
     $post = $this->input->post(NULL, TRUE);
     if ($post && $post['hdnAcao'] == 'salvar' && empty($post['hdnId'])) {
       unset($post['hdnAcao']);
